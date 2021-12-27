f9dd76e8767e5068b06d61d27e1aa952f8306262
Mira Mansour <81388088+m-mansour1@users.noreply.github.com>
2021-12-27 03:49:01 +0200

    Update Students.js

--- student-registration/client/src/components/Students.js
+++ student-registration/client/src/components/Students.js
@@ -1,37 +1,51 @@
 import React from 'react';
 import axios from 'axios';
 
-const Student = ()=>{
-    const [name, setName] = React.useState("");
-    const [standard, setStandard] = React.useState("");
-    const [rollNo, setRollNo] = React.useState([]);
+const Students = ()=>{
+    const [students, setStudents] = React.useState([]);
 
-    const onSubmitHandler = async ()=>{
-        const result = await axios.post("http://localhost:400/student",{name, standard, rollNo});
+    React.useEffect(()=>{
+        axios.get("http://localhost:4000/students")
+        .then((res)=>setStudents(res.data));
+    },[])
+
+    const onDelete = async (rollNo)=>{
+        const result = await axios.delete(`http://localhost:400/student/delete?rollNo=${rollNo}`);
         if(!result) alert("Error")
+        else
+    }
+
+    const renderTable = () =>{
+        return <>
+        <h3>List of Students</h3>
+        <table className="table">
+            <thread>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Roll No</th>
+                    <th scope="col">Class</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thread>
+            <tbody>
+                {students.map(student=>(
+                    <tr key={student.rollNo}>
+                        <td>{student.name}</td>
+                        <td>{student.rollNo}</td>
+                        <td>{student.standard}</td>
+                        <td>
+                            <button type="submit" className="btn btn-danger" onClick={()=>onDelete(student.rollNo)}>Delete</button>
+                        </td>
+                    </tr>
+
+                ))}
+            </tbody>
+        </table>
+        </>
     }
     return(
-        <form>
-            <h3>Student Registration portal</h3>
-            <div className="form-group">
-                <label>
-                    Enter student Name:
-                </label>
-                <input type="text" className="form-control" onChange={(e)=>setName(e.target.value)}/>
-            </div>
-            <div className="form-group">
-                <label>
-                    Enter student class:
-                </label>
-                <input type="text" className="form-control" onChange={(e)=>setStandard(e.target.value)}/>
-            </div>
-            <div className="form-group">
-                <label>
-                    Enter student RollNo:
-                </label>
-                <input type="number" className="form-control" onChange={(e)=>setRollNo(e.target.value)}/>
-            </div>
-            <button type="submit" className="btn btn-primary" onClick={onSubmitHandler}>Save</button>
-        </form>
+        <div>
+            {students?.length?renderTable():null}
+        </div>
     )
 }
