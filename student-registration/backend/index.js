d1ac68f661dca237f8626175d8d4e0cb6dfd8d94
Mira Mansour <81388088+m-mansour1@users.noreply.github.com>
2021-12-27 02:04:40 +0200

    Update index.js

--- student-registration/backend/index.js
+++ student-registration/backend/index.js
@@ -2,3 +2,16 @@ const express = require("express");
 const cors = require("cors");
 
 require("dotenv").config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+
+const port= process.env.PORT;
+
+app.listen(port, () =>{
+    console.log("Server is running on PORT", port);
+})
